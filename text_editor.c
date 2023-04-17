8fe8b0cbd66310dd6181160418e21f66d07375bb
Adi-vig <aditya2004cs@gmail.com>
2023-04-17 15:52:43 +0530

    most of the functions done declaring

--- text_editor.c
+++ text_editor.c
@@ -1,10 +1,62 @@
 #include<stdio.h>
+#include<string.h>
+
 #include "stack.h"
 
 int main(){
     stack* sleft = newStack(100); 
+    stack* sright= newStack(100);
+
+    char str[] = "Hello my name is Aditya";
+
+
+
+
     
 
 
     return 0;
+}
+
+
+void begin(stack* L , stack* R , char* str){
+
+    // char* output = (char*)malloc(strlen(str)*sizeof(char)+1);
+    // output[0]='|'
+
+    for(int i=0; i<strlen(str); i++){
+
+    }
+
+};
+
+
+
+
+char* showOutput(stack* L, stack* R){
+
+}
+
+
+
+void shiftCursorLeft(stack* L, stack* R){
+
+}
+
+
+void shiftCursorRight(stack* L, stack* R){
+
+}
+
+void delet(stack* L, stack* R){
+
+}
+
+void backspace(stack* L, stack* R){
+
+}
+
+
+void type(stack* L, stack* R){
+
 }
